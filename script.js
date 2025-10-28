ea7a2dccd94f702a1d3da83fbe4ef3ed93f18735
jacostales912-oss <jacotales912@gmail.com>
2025-10-28 18:06:07 +0800

    while loop

---
+++ script.js
@@ -0,0 +1,101 @@
+// let math = 92;
+// let science = 91;
+// let computer = 89;
+// let mapeh = 92;
+// let english = 85;
+
+// let average = (math + science + computer + mapeh + english)/5;
+// alert("Average : " + average);
+
+// let word1 = "Jaren Makagago wazzup man";
+// let len1 = word1.length;
+// alert(len1);
+
+// let word = " Jaren Costales ";
+// word = word.trim();
+// alert(word);
+
+// let s = "I have a dog, my dog is cute.";
+// s = s.slice(9,12);
+// alert(s);
+
+// let num1 = 5;
+// let num2 = 3;
+// let sum = num1 + num2;
+// alert(`${num1} + ${num2} = ${num1+num2}`);
+
+// let q = "Ivana";
+// let w = "Ahman";
+// let e = "she";
+// let r = "redhorse";
+
+// alert(`${q}, the friendly neighbor, waved at ${w} as ${e} walked by ${w}'s house.${w} smiled back and invited ${q} in for a cup of ${r}`);
+
+// let age = 20;
+
+// if(age >= 18){
+//     console.log("Legal Age");
+   
+// }else if(age <= 0){
+//     console.log("Invalid Age");
+// }else if(age > 0){
+//     console.log("Minor Age");
+// }
+//     console.log("Thank You")
+
+// let grade= 99;
+
+// if(grade >= 100){
+//     alert("Invalid Grade")
+// }else if(grade > 98){  
+//     alert("With Highest Honor");
+// }else if(grade >= 95){
+//     alert("With Horror")
+// }else if (grade >= 75){
+//     alert("Passed");
+// }else{
+//     alert("Failed");
+// }
+
+// let age = 32;
+// let isRegistered = false;
+
+// if(isRegistered && age >= 18)
+//     alert("Valid voter")
+
+// else if(!isRegistered && age >= 18)
+//     alert("Register fIrst")
+
+// else if(isRegistered && age < 18)
+//     alert("Invalid voter")
+     
+// else if(!isRegistered && age < 18)
+//     alert("Non voter")
+
+let day = 7;
+
+switch(day){
+    case 1:
+        alert("Monday");
+        break;
+    case 2:
+        alert("Tuesday");
+        break;
+    case 3:
+        alert("Wednesday")
+        break
+    case 4:
+        alert("Thursday");
+        break
+    case 5:
+        alert("Friday")
+        break
+    case 6:
+        alert("Saturday");
+        break
+    case 7:
+        alert("Sunday")
+        break
+    default:
+        alert("Ngekngok")
+}
